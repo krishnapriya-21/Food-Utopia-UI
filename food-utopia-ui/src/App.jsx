cc250b9c64e26740419a4b09ee021147041138ae
krishnapriya Shankar <krithikakrishnapriya@gmail.com>
2025-09-27 14:53:40 +0530

    Initial Commit

---
+++ food-utopia-ui/src/App.jsx
@@ -0,0 +1,11 @@
+
+import FigmaEmbed  from "./FigmaEmbed"
+export default function App(){
+  return(
+    <>
+    <h1>Food Utopia</h1>
+    <FigmaEmbed />
+    </>
+  )
+}
+
---
+++ food-utopia-ui/src/FigmaEmbed.jsx
@@ -0,0 +1,13 @@
+export default function FigmaEmbed(){
+    return(
+         <div style={{ width: '100%', height: '600px', border: '1px solid #ccc' }}>
+            <iframe
+            title="Food Utopia Prototype"
+            style={{ width: '100%', height: '100%', border: 'none' }}
+            src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/GUlUrd1cUDAJIPMEdfbXMF/Food-Utopia?page-id=0%3A1&node-id=0-3&scaling=scale-down&content-scaling=fixed&starting-point-node-id=0%3A3"
+            allowFullScreen
+            ></iframe>
+        </div>
+
+    )
+}
